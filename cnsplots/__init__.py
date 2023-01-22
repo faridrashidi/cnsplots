2dc06ca4ed43b66db333b56a16dbd8f20a947e25
Farid Rashidi <farid.rsh@gmail.com>
2023-01-22 14:57:33 -0500

    update

--- cnsplots/__init__.py
+++ cnsplots/__init__.py
@@ -1,4 +1,7 @@
-__version__ = "0.0.0"
+"""CNSPlots Module."""
 
 from cnsplots._altair import setup_altair
-from cnsplots._matplotlib import setup_matplotlib
+from cnsplots._matplotlib import setup_matplotlib, figure
+
+__version__ = "0.0.0"
+__all__ = (setup_altair, setup_matplotlib, figure)
--- cnsplots/_matplotlib.py
+++ cnsplots/_matplotlib.py
@@ -9,3 +9,7 @@ def setup_matplotlib():
     plt.style.core.update_nested_dict(plt.style.library, stylesheets)
     plt.style.core.available[:] = sorted(plt.style.library.keys())
     plt.style.use("CNS")
+
+
+def figure(height=150, width=150):
+    return plt.figure(figsize=(height / 72, width / 72), dpi=72)
