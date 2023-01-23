c4d6188bef3b6f0e6aa5352dab82ac3d00ad068f
Farid Rashidi <farid.rsh@gmail.com>
2023-01-22 19:54:24 -0500

    update

--- cnsplots/__init__.py
+++ cnsplots/__init__.py
@@ -1,7 +1,7 @@
 """CNSPlots Module."""
 
 from cnsplots._altair import setup_altair
-from cnsplots._matplotlib import setup_matplotlib, figure
+from cnsplots._matplotlib import figure, setup_matplotlib, barplot
 
 __version__ = "0.0.0"
-__all__ = (setup_altair, setup_matplotlib, figure)
+__all__ = (setup_altair, setup_matplotlib, figure, barplot)
--- cnsplots/_matplotlib.py
+++ cnsplots/_matplotlib.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 import cnsplots as cns
 
@@ -12,4 +13,59 @@ def setup_matplotlib():
 
 
 def figure(height=150, width=150):
-    return plt.figure(figsize=(height / 72, width / 72), dpi=72)
+    ax = plt.figure(figsize=(height / 72, width / 72), dpi=72).subplots(1)
+    ax.xaxis.labelpad = 1
+    return ax
+
+
+def barplot(df, x, y):
+    args = {
+        "edgecolor": "black",
+        "linewidth": 1,
+        "ci": None,
+        # palette=data.set_index(x)[color].to_dict() if color is not None else None,
+    }
+    g = sns.barplot(data=df, x=x, y=y, **args)
+
+    # adding values to the bars
+    groupedvalues = df.groupby(x).mean().reset_index()
+    for _, row in groupedvalues.iterrows():
+        g.text(
+            row.name,
+            row[y] + 0.05,
+            round(row[y], 2),
+            color="black",
+            ha="center",
+            rotation=0,
+            size=6,
+        )
+
+
+def distplot(color="#272822"):
+    args = {"hist_kws": {"width": 0.5, "alpha": 1.0, "color": color}}
+    return args
+
+
+def boxplot():
+    args = {
+        "boxprops": {"facecolor": "white", "edgecolor": "orange"},
+        "medianprops": {"color": "red"},
+        "whiskerprops": {"color": "blue"},
+        "capprops": {"color": "green"},
+        "flierprops": {
+            "markerfacecolor": "purple",
+            "markersize": 3,
+            "markeredgecolor": "purple",
+            "marker": "o",
+            "linewidth": 0,
+        },
+    }
+    return args
+
+
+def regplot():
+    args = {
+        "line_kws": {"color": "blue", "lw": 3},
+        "scatter_kws": {"s": 3, "color": "black", "edgecolor": "black", "alpha": 1},
+    }
+    return args
