ce8b445cd979b42c301b5fed4804aa21d0bcbcc4
Farid Rashidi <farid.rsh@gmail.com>
2023-01-24 15:06:40 -0500

    update

--- cnsplots/_seaborn.py
+++ cnsplots/_seaborn.py
@@ -1,3 +1,5 @@
+import itertools
+
 import scipy as sp
 import seaborn as sns
 from statannotations.Annotator import Annotator
@@ -5,32 +7,8 @@ from statannotations.Annotator import Annotator
 import cnsplots as cns
 
 
-def sns_barplot():
-    args = {"edgecolor": None, "linewidth": 1, "ci": None}
-    return args
-
-
-def sns_stackplot(data, x, hue, normalize=True):
-    args = {
-        "edgecolor": None,
-        "alpha": 1,
-        "shrink": 0.7,
-    }
-    if normalize:
-        ax = sns.histplot(data=data, x=x, hue=hue, multiple="fill", **args)
-        ax.set_ylabel("Frequency")
-        print("P values were determined by two-sided Fisher's exact test")
-        # TODO: calculate the p-value
-    else:
-        sns.histplot(data=data, x=x, hue=hue, multiple="stack", **args)
-
-
-def sns_histplot(data, x):
-    args = {"kde": True, "edgecolor": None}
-    sns.histplot(data=data, x=x, **args)
-
-
 def sns_boxplot(data, x, y, pairs=None, width=0.5, color=cns.colors[0]):
+    """Plot the median of y categorized by x."""
     args = {
         "showfliers": False,
         "linewidth": 0.8,
@@ -60,20 +38,60 @@ def sns_boxplot(data, x, y, pairs=None, width=0.5, color=cns.colors[0]):
         "Boxplots represent the median and bottom and upper quartiles; whiskers"
         " correspond to 1.5 times the interquartile range."
     )
+    _p_value("Mann-Whitney", data, x, ax, plotting, pairs)
 
-    if pairs is not None:
-        annotator = Annotator(ax, pairs, **plotting)
-        # https://github.com/trevismd/statannotations/blob/master/statannotations/Annotator.py#L44
-        annotator.configure(
-            test="Mann-Whitney",
-            text_format="star",  # star, simple
-            loc="inside",
-            line_width=0.8,
-            text_offset=-2,
-            color="black",
-        )
-        annotator.apply_and_annotate()
-        print("P values were determined by two-sided Mann-Whitney U test.")
+
+def sns_barplot(data, x, y, pairs=None, color=cns.colors[0], addtip=False):
+    """Plot the mean of y categorized by x."""
+    args = {
+        "edgecolor": None,
+        "linewidth": 1,
+        "ci": None,
+    }
+    plotting = {
+        "data": data,
+        "x": x,
+        "y": y,
+        "color": color,
+    }
+    plotting.update(args)
+    ax = sns.barplot(**plotting)
+    # sns.stripplot(data=data, x=x, y=y, size=3, linewidth=0)
+
+    if addtip:
+        groupedvalues = data.groupby(x).mean().reset_index()
+        for _, row in groupedvalues.iterrows():
+            ax.text(
+                row.name,
+                row[y] + 0.05,
+                round(row[y], 2),
+                color="black",
+                ha="center",
+                rotation=0,
+                size=6,
+            )
+
+    _p_value("t-test_welch", data, x, ax, plotting, pairs)
+
+
+def sns_stackplot(data, x, hue, normalize=True):
+    args = {
+        "edgecolor": None,
+        "alpha": 1,
+        "shrink": 0.7,
+    }
+    if normalize:
+        ax = sns.histplot(data=data, x=x, hue=hue, multiple="fill", **args)
+        ax.set_ylabel("Frequency")
+        # TODO: calculate the p-value
+        print("P values were determined by two-sided Fisher's exact test")
+    else:
+        sns.histplot(data=data, x=x, hue=hue, multiple="stack", **args)
+
+
+def sns_histplot(data, x):
+    args = {"kde": True, "edgecolor": None}
+    sns.histplot(data=data, x=x, **args)
 
 
 def sns_regplot(data, x, y):
@@ -96,3 +114,25 @@ def plt_piechart(data, x, order=None):
         explode=[0] * df.shape[0],
         textprops={"fontsize": 6, "color": "white"},
     )
+
+
+def _p_value(test, data, x, ax, plotting, pairs):
+    if pairs == "all":
+        pairs = list(itertools.combinations(data[x].unique(), 2))
+    annotator = Annotator(ax, pairs, **plotting)
+    # https://github.com/trevismd/statannotations/blob/master/statannotations/Annotator.py#L44
+    annotator.configure(
+        test=test,
+        text_format="full",  # star, simple
+        loc="inside",
+        line_width=0.8,
+        text_offset=0.5,
+        color="black",
+        show_test_name=False,
+        pvalue_format_string="{:.1e}",
+    )
+    annotator.apply_and_annotate()
+    if test == "Mann-Whitney":
+        print("P values were determined by two-sided Mann-Whitney U test.")
+    if test == "t-test_welch":
+        print("P values were determined by two-sided Welch's t-test.")
