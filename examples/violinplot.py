feabe5d1545904ac8379719375e0429c3043ec30
Farid Rashidi <farid.rsh@gmail.com>
2023-02-01 08:44:56 -0500

    update

---
+++ examples/violinplot.py
@@ -0,0 +1,26 @@
+"""
+Construct lienage tree using Trisicell-Boost
+--------------------------------------------
+
+This example shows how to construct a lineage tree using Trisicell-Boost on a binary
+single-cell genotype matrix.
+"""
+
+# sphinx_gallery_thumbnail_path = "_static/thumbnails/trisicell-boost.png"
+
+import cnsplots as cns
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+cns.setup_matplotlib()
+
+# %%
+# First, we load a binary test single-cell genotype data.
+tips = sns.load_dataset("tips")
+
+# %%
+# Next, using :func:`trisicell.tl.booster` we remove the single-cell noises from the
+# input.
+cns.figure(150, 100)
+cns.violinplot(data=tips, x="day", y="total_bill", pairs="all")
+plt.savefig('test.pdf')
