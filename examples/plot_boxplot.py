ed51b61d4281eb947e24b0a381cfaa97029d3628
Farid Rashidi <farid.rsh@gmail.com>
2023-12-19 10:20:02 -0500

    update

--- examples/plot_boxplot.py
+++ examples/plot_boxplot.py
@@ -18,6 +18,7 @@ tips = sns.load_dataset("tips")
 # plot boxplot using :func:`cnsplots.boxplot`
 cns.figure(150, 100)
 cns.boxplot(data=tips, x="day", y="total_bill")
+plt.xticks(rotation=40, ha="right", rotation_mode="anchor")
 
 # %%
 # plot boxplot using :func:`cnsplots.boxplot`
