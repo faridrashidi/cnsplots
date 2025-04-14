212d30f966e0248aaab39d736b45a7700a6d64d4
Farid Rashidi <farid.rsh@gmail.com>
2025-04-14 16:57:37 -0400

    update

--- cnsplots/_setup.py
+++ cnsplots/_setup.py
@@ -13,7 +13,8 @@ def setup_matplotlib(
     def config():
         return {
             "mathtext.fontset": "custom",
-            "font.family": ["Helvetica", "DejaVu Sans", "sans-serif"],
+            "font.family": "sans-serif",
+            "font.sans-serif": "Helvetica",
             "font.size": fontsize_title,
             "savefig.bbox": "tight",
             "savefig.pad_inches": 0.01,
