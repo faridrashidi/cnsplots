085d46b335fd267760bfa3246f58d37e5d091743
Farid Rashidi <farid.rsh@gmail.com>
2025-09-26 18:24:54 -0400

    update

--- examples/plot_x_upsetplot.py
+++ examples/plot_x_upsetplot.py
@@ -11,9 +11,25 @@ import upsetplot as usp
 
 import cnsplots as cns
 
-example = usp.generate_counts()
+example1 = usp.generate_counts()
+sets = {
+    "A": {"a", "b", "c", "d"},
+    "B": {"c", "d", "e", "f"},
+    "C": {"b", "d", "f", "g"},
+    "D": {"a", "e", "g", "h"},
+}
+memberships = []
+for item in set.union(*sets.values()):
+    membership = [name for name, s in sets.items() if item in s]
+    memberships.append(membership)
+example2 = usp.from_memberships(memberships)
 
 
 # %%
 # plot upsetplot using :func:`cnsplots.upsetplot`
-cns.upsetplot(example, element_size=30, show_counts="{:,}", totals_plot_elements=0)
+cns.upsetplot(example1, element_size=30, show_counts="{:,}", totals_plot_elements=0)
+
+
+# %%
+# plot upsetplot using :func:`cnsplots.upsetplot`
+cns.upsetplot(example2, element_size=30, show_counts="{:,}", subset_size="count")
