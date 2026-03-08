1a8bb90428d9ffaaa4632519b8ff35b78ad9a839
Farid Rashidi <farid.rsh@gmail.com>
2026-03-07 21:20:48 -0500

    🔧 Update Sphinx build command to use 'uv run' for improved execution

--- docs/make.bat
+++ docs/make.bat
@@ -5,7 +5,7 @@ pushd %~dp0
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+	set SPHINXBUILD=uv run --extra doc sphinx-build
 )
 set SOURCEDIR=.
 set BUILDDIR=build
@@ -13,13 +13,13 @@ set BUILDDIR=build
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
+	echo.The documentation build command was not found. Make sure you have uv
+	echo.installed, or set the SPHINXBUILD environment variable to point to a
+	echo.working Sphinx command. Alternatively you may add the required
+	echo.executables to PATH.
 	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.https://www.sphinx-doc.org/
+	echo.If you don't have uv installed, grab it from
+	echo.https://docs.astral.sh/uv/
 	exit /b 1
 )
 
