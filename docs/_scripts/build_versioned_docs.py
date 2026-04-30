4b289af2785bb0a1d27a65ec171dfc40c4ec71b4
Farid Rashidi <farid.rsh@gmail.com>
2026-04-30 18:45:09 -0400

    🔥 Remove Moranga font references from stylesheets

--- docs/_scripts/build_versioned_docs.py
+++ docs/_scripts/build_versioned_docs.py
@@ -124,7 +124,6 @@ def _render_root_404_page() -> str:
     <link rel="stylesheet" type="text/css" href="$furo_extensions_css_src">
     <link rel="stylesheet" type="text/css" href="$override_css_src">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preconnect" href="https://fonts.prefix.dev" crossorigin>
     <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=JetBrains+Mono:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
